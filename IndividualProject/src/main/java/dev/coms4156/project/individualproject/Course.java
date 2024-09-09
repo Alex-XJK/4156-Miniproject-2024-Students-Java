b6beb0881df0af939b26c7907e9e63d8d681244a
Restyled.io <commits@restyled.io>
2024-09-09 14:45:24 +0000

    Restyled by clang-format

--- IndividualProject/src/main/java/dev/coms4156/project/individualproject/Course.java
+++ IndividualProject/src/main/java/dev/coms4156/project/individualproject/Course.java
@@ -11,14 +11,17 @@ import java.io.Serializable;
 public class Course implements Serializable {
 
   /**
-   * Constructs a new Course object with the given parameters. Initial count starts at 0.
+   * Constructs a new Course object with the given parameters. Initial count
+   * starts at 0.
    *
    * @param instructorName     The name of the instructor teaching the course.
    * @param courseLocation     The location where the course is held.
    * @param timeSlot           The time slot of the course.
-   * @param capacity           The maximum number of students that can enroll in the course.
+   * @param capacity           The maximum number of students that can enroll in
+   *     the course.
    */
-  public Course(String instructorName, String courseLocation, String timeSlot, int capacity) {
+  public Course(String instructorName, String courseLocation, String timeSlot,
+                int capacity) {
     this.courseLocation = courseLocation;
     this.instructorName = instructorName;
     this.courseTimeSlot = timeSlot;
@@ -52,54 +55,38 @@ public class Course implements Serializable {
     return false;
   }
 
+  public String getCourseLocation() { return this.courseLocation; }
 
-  public String getCourseLocation() {
-    return this.courseLocation;
-  }
+  public String getInstructorName() { return this.instructorName; }
 
-
-  public String getInstructorName() {
-    return this.instructorName;
-  }
-
-
-  public String getCourseTimeSlot() {
-    return this.courseTimeSlot;
-  }
+  public String getCourseTimeSlot() { return this.courseTimeSlot; }
 
   @Override
   public String toString() {
-    return "\nInstructor: " + instructorName +  "; Location: "  + courseLocation +  "; "
+    return "\nInstructor: " + instructorName + "; Location: " + courseLocation +
+        "; "
         + "Time: " + courseTimeSlot;
   }
-
 
   public void reassignInstructor(String newInstructorName) {
     this.instructorName = newInstructorName;
   }
 
-
   public void reassignLocation(String newLocation) {
     this.courseLocation = newLocation;
   }
 
-
-  public void reassignTime(String newTime) {
-    this.courseTimeSlot = newTime;
-  }
-
+  public void reassignTime(String newTime) { this.courseTimeSlot = newTime; }
 
   public void setEnrolledStudentCount(int count) {
     this.enrolledStudentCount = count;
   }
 
-
   public boolean isCourseFull() {
     return this.enrollmentCapacity <= this.enrolledStudentCount;
   }
 
-  @Serial
-  private static final long serialVersionUID = 123456L;
+  @Serial private static final long serialVersionUID = 123456L;
   private final int enrollmentCapacity;
   private int enrolledStudentCount;
   private String courseLocation;
--- IndividualProject/src/test/java/dev/coms4156/project/individualproject/CourseUnitTests.java
+++ IndividualProject/src/test/java/dev/coms4156/project/individualproject/CourseUnitTests.java
@@ -1,8 +1,8 @@
 package dev.coms4156.project.individualproject;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -21,10 +21,10 @@ public class CourseUnitTests {
     testCourse = new Course("Griffin Newbold", "417 IAB", "11:40-12:55", 250);
   }
 
-
   @Test
   public void toStringTest() {
-    String expectedResult = "\nInstructor: Griffin Newbold; Location: 417 IAB; Time: 11:40-12:55";
+    String expectedResult =
+        "\nInstructor: Griffin Newbold; Location: 417 IAB; Time: 11:40-12:55";
     assertEquals(expectedResult, testCourse.toString());
   }
 
@@ -55,4 +55,3 @@ public class CourseUnitTests {
   /** The test course instance used for testing. */
   public static Course testCourse;
 }
-
